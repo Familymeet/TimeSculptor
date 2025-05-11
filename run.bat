c9238229dbf5263dc8bf57062749278f73936fb6
Melvin D Hillard III <mhillard@lewisu.edu>
2025-05-11 18:02:33 -0500

    update files

--- run.bat
+++ run.bat
@@ -1,8 +1,21 @@
 @echo off
-python -m venv venv
-call venv\Scripts\activate
+REM Create virtual environment if it doesn't exist
+IF NOT EXIST "venv" (
+    python -m venv venv
+)
+
+REM Activate the virtual environment
+call venv\Scripts\activate.bat
+
+REM Upgrade pip and install dependencies
+python -m pip install --upgrade pip
 pip install -r requirements.txt
+
+REM Set environment variables
 set FLASK_APP=app.py
 set FLASK_ENV=development
-flask run
+
+REM Run Flask
+python -m flask run
+
 pause
